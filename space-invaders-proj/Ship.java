c4924aca4c7ffb2d7af32f56ba1437feff36faba
unknown <matthewbenz24@gmail.com>
2018-10-25 15:07:06 -0500

    added ship actor

---
+++ space-invaders-proj/Ship.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Ship here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ship extends Actor
+{
+    /**
+     * Act - do whatever the Ship wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public Ship() {
+        GreenfootImage shipImage = new GreenfootImage("Ship.png");
+        shipImage.getHeight();
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
